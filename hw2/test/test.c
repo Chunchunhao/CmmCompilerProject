21e53a4a160e4dd3d85abc35c663862c7fef8a82
俊豪 王 <pika@junhaodeMacBook-Air.local>
2014-10-26 23:42:28 +0800

    hw2 v0

---
+++ hw2/test/test.c
@@ -0,0 +1,30 @@
+int func(int a, int b)
+{
+	int main;
+    a = 3;
+	main = a * b;
+}
+
+int main()
+{
+	/* 1: this is just a sample */
+    int c, d;
+	float f,g;
+	g = 3.3;
+    "c hi hi hi" = 1;
+    d = 3;
+	/* 2: this is a 3-line
+	comment
+	*/
+
+	a === c;
+	a += c;
+	""""""""""""""
+
+	f = read();
+    func(c,e);
+	write(d);
+	d = 0;  /* 3: this is /* the end ***/
+
+    return 0;
+}
